2473a971666d003176945eb76b2ff5bdcb45a4a8
Buletiss <vinicius1997feitosa@gmail.com>
2025-02-02 22:33:58 -0300

    pagina simples de profile onde mostra o email phone, username e imagem

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,29 @@
+import "@/app/globals.css";
+import HandleLogout from "@/components/handleLogout";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { getUserAuth } from "@/hooks/getUserAuth";
+
+export default function Profile() {
+  const { user } = getUserAuth();
+
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-rich-blue">
+      <div className="bg-eggshell w-1/2 h-1/2 rounded-xl flex flex-col items-center justify-evenly">
+        <Avatar className="w-16 h-16 ">
+          <AvatarImage src={user?.image} alt="avatar" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <text className="text-black">{user?.username}</text>
+        <text className="text-black">{user?.email}</text>
+        <text className="text-black">{user?.phone}</text>
+        <div>
+          <Button variant="outline">
+            <a href="/menu">Menu</a>
+          </Button>
+          <HandleLogout />
+        </div>
+      </div>
+    </div>
+  );
+}
