d5aa3a92334d816f0a9c9a030fdc1cdd7e431e97
Buletiss <vinicius1997feitosa@gmail.com>
2025-02-02 22:25:03 -0300

    mock from side-side-bar, esqueci de puxar via API e então criei um mock para otimizar tempo

---
+++ public/mocks/dataMock.ts
@@ -0,0 +1,111 @@
+export const dataNavMain = {
+  navMain: [
+    {
+      title: "Articles",
+      url: "#",
+      items: [
+        {
+          title: "Articles",
+          url: "/menu",
+        },
+      ],
+    },
+
+    {
+      title: "Fiction",
+      url: "#",
+      items: [
+        {
+          title: "Combined Print & E-Book Fiction",
+          url: "combined-print-and-e-book-fiction",
+        },
+        {
+          title: "Hardcover Fiction",
+          url: "hardcover-fiction",
+        },
+        {
+          title: "Paperback Trade Fiction",
+          url: "trade-fiction-paperback",
+        },
+      ],
+    },
+    {
+      title: "NONFICTION",
+      url: "#",
+      items: [
+        {
+          title: "Combined Print & E-Book Nonfiction",
+          url: "combined-print-and-e-book-nonfiction",
+        },
+        {
+          title: "Hardcover Nonfiction",
+          url: "hardcover-nonfiction",
+        },
+        {
+          title: "Paperback Nonfiction",
+          url: "paperback-nonfiction",
+        },
+        {
+          title: "Advice, How-To Miscellaneous",
+          url: "advice-how-to-and-miscellaneous",
+        },
+      ],
+    },
+    {
+      title: "Childrens",
+      url: "#",
+      items: [
+        {
+          title: "Childrens Middle Grade Hardcover",
+          url: "childresn-middle-grade-hardcover",
+        },
+        {
+          title: "Childrens Picture Books",
+          url: "picture-books",
+        },
+        {
+          title: "Young Adult",
+          url: "young-adult-hardcover",
+        },
+        {
+          title: "Children's & Young Adult Series",
+          url: "series-books",
+        },
+      ],
+    },
+    {
+      title: "Monthly Lists",
+      url: "#",
+      items: [
+        {
+          title: "Audio Fiction",
+          url: "audio-fiction",
+        },
+        {
+          title: "Audio Nonfiction",
+          url: "audio-nonfiction",
+        },
+        {
+          title: "Business",
+          url: "business-books",
+        },
+        {
+          title: "Graphic Books & Manga",
+          url: "graphic-books-and-manga",
+        },
+        {
+          title: "Mass Market",
+          url: "mass-market-monthly",
+        },
+        {
+          title: "Middle Grade Paperback",
+          url: "middle-grade-paperback-monthly",
+        },
+        {
+          title: "Young Adult Paperback",
+          url: "young-adult-paperback-monthly",
+        },
+      ],
+    },
+  ],
+};
